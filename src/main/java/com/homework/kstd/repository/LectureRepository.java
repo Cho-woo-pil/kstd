f5107534805738defafb8ff0253220daecff95f4
mycom <jwp4465@naver.com>
2024-01-22 21:55:06 +0900

    feat: 강의 등록 API 구현

--- src/main/java/com/homework/kstd/repository/LectureRepository.java
+++ src/main/java/com/homework/kstd/repository/LectureRepository.java
@@ -2,6 +2,15 @@ package com.homework.kstd.repository;
 
 import com.homework.kstd.entity.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public interface LectureRepository extends JpaRepository<Lecture, UUID> {
+    @Query("SELECT COUNT(l) = 0 FROM Lecture l " +
+            "WHERE l.venueId = :venueId " +
+            "AND ((l.startTime >= :endTime AND l.endTime <= :startTime) " +
+            "OR (l.endTime >= :startTime AND l.startTime <= :endTime))")
+    boolean venueAvailable(String venueId, LocalDateTime startTime, LocalDateTime endTime);
 }
