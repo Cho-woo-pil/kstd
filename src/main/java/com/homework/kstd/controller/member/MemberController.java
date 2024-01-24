ce5f1bd020c350781cf34ab0ca6e1a8dda047f3a
mycom <jwp4465@naver.com>
2024-01-24 20:56:25 +0900

    fix: 내가신청한 강의 조회 API 구현

--- src/main/java/com/homework/kstd/controller/member/MemberController.java
+++ src/main/java/com/homework/kstd/controller/member/MemberController.java
@@ -1,9 +1,9 @@
 package com.homework.kstd.controller.member;
 
 import com.homework.kstd.dto.ApplyLectureDto;
-import com.homework.kstd.dto.RegistorLectureDto;
 import com.homework.kstd.service.member.ApplyLectureService;
 import com.homework.kstd.service.member.CancleLectureService;
+import com.homework.kstd.service.member.GetApplyHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +15,7 @@ public class MemberController {
 
     private final ApplyLectureService applyLectureService;
     private final CancleLectureService cancleLectureService;
+    private final GetApplyHistoryService getApplyHistoryService;
     @PostMapping("/apply")
     public ResponseEntity<?> registorLecture(@RequestBody ApplyLectureDto dto) {
 
@@ -24,5 +25,10 @@ public class MemberController {
     @PatchMapping("/cancle")
     public ResponseEntity<?> cancelLectureApplication(@RequestBody ApplyLectureDto dto) {
         return cancleLectureService.cancelLecture(dto);
+    }
+
+    @GetMapping("/apply/history/{employeeId}")
+    public ResponseEntity<?> getApplyHistory(@PathVariable String employeeId) {
+        return getApplyHistoryService.getApplyHistory(employeeId);
     }
 }
